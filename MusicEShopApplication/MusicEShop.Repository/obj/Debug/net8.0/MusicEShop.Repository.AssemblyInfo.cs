83b12140dd06ae50b3c999252e49d4326ca0bfda
BiseraG <biseragaleska@gmail.com>
2025-02-10 17:05:03 +0100

    -

--- MusicEShopApplication/MusicEShop.Repository/obj/Debug/net8.0/MusicEShop.Repository.AssemblyInfo.cs
+++ MusicEShopApplication/MusicEShop.Repository/obj/Debug/net8.0/MusicEShop.Repository.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicEShop.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02cbfaadf5290de3a0c97cfa74114e07f200578a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9943602707e12ece491de1e9ed25323fcceec783")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEShop.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEShop.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
